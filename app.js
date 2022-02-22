a2d5afb77f5a211bd5e90350110b35f8a304e38b
jsingh <jsingh@skytrac.ca>
2022-02-22 08:51:37 -0800

    changes made to start the server only after database connection

--- app.js
+++ app.js
@@ -16,56 +16,53 @@ const uri = "mongodb+srv://madhulika:me%40A2173@cluster0.hmdpb.mongodb.net/test?
 const client = new MongoClient(uri);
 // connect to your MongoDB database through your URI. 
 // The connect() function takes a uri and callback function as arguments.
-var collection=null;
 client.connect()
-.then (()=>{
-    collection = client.db("test").collection("users");
-})
-.catch(err=>{
+  .then(() => {
+    const collection = client.db("test").collection("users");
+    app.get("/:point", (req, res) => {
+
+      collection.find({}).toArray((err, docs) => {
+        if (err) {
+          // if an error happens
+          res.send("Error in GET req.");
+        } else {
+          // if all works
+          //res.send(docs); //check
+          res.render('index', { docs: docs });
+          docs.forEach(element => {
+            console.log(element.points);
+          });
+
+        }
+      });
+    });
+    // Responds to POST requests with the route parameter being the username.
+    // Creates a new user in the collection with the `user` parameter and the JSON sent with the req in the `body` property
+    app.post("/:point", (req, res) => {
+      // inserts a new document in the database (collection)
+      //console.log("in Post\n");
+      //console.log(req.body);
+      //console.log(JSON.stringify(req.body));
+      const product = req.body;
+      collection.insertOne(
+        product)
+        .then( // this is one object to insert. `requst.params` gets the url req parameters
+          () => {
+            res.send("Information inserted");
+          })
+        .catch(err => {
+          client.close();
+          console.log(err);
+          res.send("Error in Post");
+        })
+    });
+
+    // listen for requests
+    var listener = app.listen(port, () => {
+      console.log("Your app is listening on port " + listener.address().port);
+    });
+  })
+  .catch(err => {
     client.close();
     console.log(err);
-})
-  app.get("/:point", (req, res) => {
-
-     collection.find({}).toArray((err, docs) => {
-      if (err) {
-        // if an error happens
-        res.send("Error in GET req.");
-      } else {
-        // if all works
-        //res.send(docs); //check
-        res.render('index',{docs:docs});
-        docs.forEach(element => {
-            console.log(element.points);
-        });
-        
-      }
-    }); 
   });
-
-  // Responds to POST requests with the route parameter being the username.
-  // Creates a new user in the collection with the `user` parameter and the JSON sent with the req in the `body` property
-  app.post("/:point", (req, res) => {
-    // inserts a new document in the database (collection)
-    //console.log("in Post\n");
-    //console.log(req.body);
-    //console.log(JSON.stringify(req.body));
-    const product=req.body;
-    collection.insertOne(
-      product)
-      .then( // this is one object to insert. `requst.params` gets the url req parameters
-      () => {
-          res.send("Information inserted");
-      })
-    .catch(err=>{
-        client.close();
-        console.log(err);
-        res.send("Error in Post");
-    })
-  });
-
-  // listen for requests
-  var listener = app.listen(port, () => {
-    console.log("Your app is listening on port " + listener.address().port);
-  });
-//});
